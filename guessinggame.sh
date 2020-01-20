b8c25c8b1b7f417e2fd96c37d2c4d74a44f9133d
andreismol <andreismolnikov00@gmail.com>
2020-01-20 14:09:35 +1100

    Made a few changes to guessinggame.sh such that it checked input before proceeding.

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,28 +4,34 @@
 echo "Please guess how many files are in the current directory."
 read guess
 
-function filetotal {
+if ! [[ $guess =~ ^[0-9]+$ ]]
+        then
+                echo "Sorry, that's not a positive integer. Please enter a whole, positive number."
+        else
 
-	ls $1 | wc -l
+	function filetotal {
 
-}
+		ls $1 | wc -l
 
-totalfiles=$(filetotal)
+	}
 
-while [[ $guess -ne $totalfiles ]]
-do
+	totalfiles=$(filetotal)
 
-	if [[ $guess -gt $totalfiles ]]
+	while [[ $guess -ne $totalfiles ]]
+	do
+
+		if [[ $guess -gt $totalfiles ]]
+		then
+			echo "Sorry! Your guess was too high. Please try again."
+		elif [[ $guess -lt $totalfiles ]]
+		then
+			echo "Sorry! Your guess was too low. Please try again."
+		fi
+		read guess
+	done
+
+	if [[ $guess -eq $totalfiles ]]
 	then
-		echo "Sorry! Your guess was too high. Please try again."
-	elif [[ $guess -lt $totalfiles ]]
-	then
-		echo "Sorry! Your guess was too low. Please try again."
+	        echo "Congratulations! You're right!"
 	fi
-	read guess
-done
-
-if [[ $guess -eq $totalfiles ]]
-then
-        echo "Congratulations! You're right!"
 fi
